4fd90f9176a63a8f87da0f3a7e4c9104be730b87
zar1k <andreyzarazka@gmail.com>
2016-02-10 15:56:25 +0200

    Fixed bugs

--- src/main/java/com/qoobico/remindme/server/controller/ReminderController.java
+++ src/main/java/com/qoobico/remindme/server/controller/ReminderController.java
@@ -1,10 +1,12 @@
 package com.qoobico.remindme.server.controller;
 
+import com.qoobico.remindme.server.entity.Remind;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
 
 @Controller
 @RequestMapping("/reminder")
@@ -12,7 +14,17 @@ public class ReminderController {
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
-    public String getReminder(ModelMap model){
-        return "May reminder";
+    public Remind getReminder() {
+        return createMockRemid();
     }
+
+    private Remind createMockRemid() {
+        Remind remind = new Remind();
+        remind.setId(1);
+        remind.setRemindDate(new Date());
+        remind.setTitle("My first remind");
+
+        return remind;
+    }
+
 }
